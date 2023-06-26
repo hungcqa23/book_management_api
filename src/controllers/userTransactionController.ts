b74d47f762037e0434646119200c288242dd2905
An Hưng <anbeel191@gmail.com>
2023-06-27 01:01:13 +0700

    Add feeReceipt Controller.js

--- src/controllers/userTransactionController.ts
+++ src/controllers/userTransactionController.ts
@@ -5,37 +5,35 @@ import UserTransaction from '../models/userTransaction';
 import { AuthRequest, IUserFinancials, IUserTransaction } from '../interfaces/model.interfaces';
 import UserFinancials from '../models/userFinancials';
 
-const updateStatusTransaction = catchAsync(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    if (!req.query.status || !req.query.user) {
-      return next(new AppError(`Transaction not found`, 404));
-    }
-
-    const { status, user } = req.query;
-    if (req.user.id != user) {
-      return next(new AppError(`Transaction doesn't belong to this user`, 400));
-    }
-    const userFinancials: IUserFinancials | null = await UserFinancials.findOne({ user });
-    if (!userFinancials) {
-      return next(new AppError(`Please create a User Financials!`));
-    }
-
-    const updatedTransaction: IUserTransaction | null = await UserTransaction.findOne({
-      userFinancials: userFinancials._id
-    });
-
-    if (!updatedTransaction) {
-      return next(new AppError(`Not found user transaction!`));
-    }
-
-    updatedTransaction.status = 'success';
-    updatedTransaction.save();
-
-    res.status(200).json({
-      status,
-      date: updatedTransaction
-    });
+const updateStatusTransaction = catchAsync(async (req: AuthRequest, res: Response, next: NextFunction) => {
+  if (!req.query.status || !req.query.user) {
+    return next(new AppError(`Transaction not found`, 404));
   }
-);
+
+  const { status, user } = req.query;
+  if (req.user.id != user) {
+    return next(new AppError(`Transaction doesn't belong to this user`, 400));
+  }
+  const userFinancials: IUserFinancials | null = await UserFinancials.findOne({ user });
+  if (!userFinancials) {
+    return next(new AppError(`Please create a User Financials!`));
+  }
+
+  const updatedTransaction: IUserTransaction | null = await UserTransaction.findOne({
+    userFinancials: userFinancials._id
+  }).sort({ createdAt: -1 });
+
+  if (!updatedTransaction) {
+    return next(new AppError(`Not found user transaction!`));
+  }
+
+  updatedTransaction.status = 'success';
+  updatedTransaction.save();
+
+  res.status(200).json({
+    status,
+    date: updatedTransaction
+  });
+});
 
 export default { updateStatusTransaction };
--- src/routes/feeReceipt.routes.ts
+++ src/routes/feeReceipt.routes.ts
@@ -7,16 +7,16 @@ import catchAsync from '../utils/catchAsync';
 import AppError from '../utils/appError';
 const router = Router();
 
-const setUserFinancials = catchAsync(
-  async (req: AuthRequest, res: Response, next: NextFunction) => {
-    const userFinancials: IUserFinancials | null = await UserFinancials.findById(req.user.id);
-    if (!userFinancials) {
-      return next(new AppError(`Please login with account!`));
-    }
-    req.body.userFinancials = userFinancials.id;
-    next();
+const setUserFinancials = catchAsync(async (req: AuthRequest, res: Response, next: NextFunction) => {
+  const userFinancials: IUserFinancials | null = await UserFinancials.findById({
+    user: req.user.id
+  });
+  if (!userFinancials) {
+    return next(new AppError(`Please login with account!`));
   }
-);
+  req.body.userFinancials = userFinancials.id;
+  next();
+});
 
 router.use(authController.protect);
 router
